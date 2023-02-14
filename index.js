a22509ef7bf4eed9ba2eb837a60e68a7712ee20c
YimmyC <jim.cassano1@gmail.com>
2023-02-14 10:28:16 -0600

    choice workin

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const inquirer = require("inquirer");
 
-inquirer.prompt;
+inquirer
+  .prompt([
+    {
+      type: "list",
+      message: "What would you like to do?",
+      name: "choice",
+      choices: [{ name: "View all departments", value: "VIEW_DEPARTMENTS" }],
+    },
+  ])
+  .then((response) => {
+    console.log(response.choice);
+  });
